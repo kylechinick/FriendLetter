6e2dc4698c15b88b52186ad25217ec7d583946c3
Kyle Chinick <kylechinick@icloud.com>
2022-07-18 10:29:45 -0700

    Add support for new LetterVariable properties: Souvenir and Location
    Co-authored-by : Zhibin Liang <ifthereisoneday@gmail.com>
    Co-authored-by: Daniel Ware <waredanielb@gmail.com>

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -24,11 +24,13 @@ namespace FriendLetter.Controllers
   
 
   [Route("/postcard")]
-  public ActionResult Postcard(string recipient, string sender)
+  public ActionResult Postcard(string recipient, string sender, string location, string souvenir)
   {
     LetterVariable myLetterVariable = new LetterVariable();
     myLetterVariable.Recipient = recipient;
     myLetterVariable.Sender = sender;
+    myLetterVariable.Location = location;
+    myLetterVariable.Souvenir = souvenir;
     return View(myLetterVariable);
   }
 }
